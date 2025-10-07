8bcff9657e496bf76fd6505af4039ec0dcbff1b0
deepthi-1n <ndeepthinagarajbca24@rvu.edu.in>
2025-10-07 14:59:44 +0530

    Update Calculator.java

--- LAB2/Calculator.java
+++ LAB2/Calculator.java
@@ -1,15 +1,21 @@
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+class Calculator {
+    int add(int a, int b){
+        return a+b;
     }
-
-    public int subtract(int a, int b) {
-        return a - b;
+    int subtract(int a, int b){
+        return a-b;
     }
-
+    int multiply(int a, int b){
+        return a*b;
+    }
+    int divide(int a, int b){
+        return a/b;
+    }
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println("Addition : " + calc.add(10, 5));
-        System.out.println("Subtraction : " + calc.subtract(10, 5));
+        Calculator calc=new Calculator();
+        System.out.println("Addition: "+ calc.add(10,5));
+        System.out.println("Subtraction: "+ calc.subtract(10,5));
+        System.out.println("Multiplication: "+ calc.multiply(10,5));
+        System.out.println("Division: "+ calc.divide(10,5));
     }
 }
